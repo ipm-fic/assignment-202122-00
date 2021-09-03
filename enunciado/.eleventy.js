ebcb0d4690e2b2f9c8f846cd82bfd0d7b3acc40e
David Cabrero Souto <david.cabrero@madsgroup.org>
2021-09-03 17:11:33 +0200

    CHANGED: abs urls to rel ones

--- enunciado/.eleventy.js
+++ enunciado/.eleventy.js
@@ -2,15 +2,25 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("vendor");
-  eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("vendor");
+    eleventyConfig.addPassthroughCopy("img");
 
-  eleventyConfig.addPlugin(syntaxHighlight);
-
-  return {
-    dir: {
-      output: "../docs"
+    eleventyConfig.addPlugin(syntaxHighlight);
+    
+    eleventyConfig.addFilter("baseUrl", function(value) {
+	if (value == "/") {
+	    return "./";
+	}
+	else {
+	    depth = value.split("/").length - 1;
+	    return "../".repeat(depth);
+	}
+    });
+	
+    return {
+	dir: {
+	    output: "../docs"
+	}
     }
-  }
 };
